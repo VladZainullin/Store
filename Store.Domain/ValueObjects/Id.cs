745a04e9f475092920002da67c0f6e21c2f8b464
Vladislav Zainullin <mittoface02@mail.ru>
2023-04-05 22:42:31 +0300

    Cleanup

--- Store.Domain/ValueObjects/Id.cs
+++ Store.Domain/ValueObjects/Id.cs
@@ -2,12 +2,12 @@ namespace Store.Domain.ValueObjects;
 
 public sealed class Id<T>
 {
-    public T Value { get; }
-
     private Id(T value)
     {
         Value = value;
     }
+
+    public T Value { get; }
 
     public static implicit operator Id<T>(T value)
     {
