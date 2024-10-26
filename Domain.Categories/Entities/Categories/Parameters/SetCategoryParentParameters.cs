60d081c78ba0c2494fd97ee635b25bddaef2a2c0
Vladislav Zainullin <mittoface02@mail.ru>
2024-10-26 16:17:30 +0300

    Удаляет неиспользуемый код

--- Domain.Categories/Entities/Categories/Parameters/SetCategoryParentParameters.cs
+++
@@ -1,9 +0,0 @@
-// ReSharper disable UnusedAutoPropertyAccessor.Global
-namespace Domain.Categories.Entities.Categories.Parameters;
-
-public readonly struct SetCategoryParentParameters
-{
-    public required Category Parent { get; init; }
-
-    public required TimeProvider TimeProvider { get; init; }
-}
