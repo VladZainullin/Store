773f543a972300c6c86e5bc1b9a7fd162689f835
Vladislav Zainullin <mittoface02@mail.ru>
2024-11-23 21:50:13 +0300

    Удаляет запрос получения логотипа категории

--- Application.Contracts/Features/Categories/Commands/UpdateCategoryLogo/UpdateCategoryLogoCommand.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-
-namespace Application.Contracts.Features.Categories.Commands.UpdateCategoryLogo;
-
-public sealed record UpdateCategoryLogoCommand(
-    UpdateCategoryLogoRequestRouteDto RouteDto,
-    UpdateCategoryLogoRequestFormDto FormDto) : IRequest;
