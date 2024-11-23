fceec258c578dff50f10758c2447198cf97bcf1e
Vladislav Zainullin <mittoface02@mail.ru>
2024-11-23 20:39:37 +0300

    Дополняет конфигурацию продукта

--- Persistence/Configurations/ProductConfiguration.cs
+++ Persistence/Configurations/ProductConfiguration.cs
@@ -9,10 +9,10 @@ internal sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
     public void Configure(EntityTypeBuilder<Product> builder)
     {
         builder.Property(static m => m.Id).HasField("_id").ValueGeneratedNever();
-        builder.Property(static m => m.Title).HasField("_title");
+        builder.Property(static m => m.Title).HasField("_title").HasMaxLength(200);
         builder.Property(static p => p.Cost).HasField("_cost");
         builder.Property(static p => p.Quantity).HasField("_quantity");
-        builder.Property(static m => m.Description).HasField("_description");
+        builder.Property(static m => m.Description).HasField("_description").HasMaxLength(6000);
         builder.Property(static m => m.UpdatedAt).HasField("_updatedAt");
         builder.Property(static m => m.CreatedAt).HasField("_createdAt");
         builder.Property(static m => m.RemovedAt).HasField("_removedAt");
