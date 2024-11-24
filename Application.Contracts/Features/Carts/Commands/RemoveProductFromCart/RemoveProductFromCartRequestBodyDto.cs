1273f6bd588eec2da8608936c88d7748ea5cd0c5
Vladislav Zainullin <mittoface02@mail.ru>
2024-11-24 13:30:43 +0300

    Исправляет запрос удаления категории

--- Application.Contracts/Features/Carts/Commands/RemoveProductFromCart/RemoveProductFromCartRequestBodyDto.cs
+++ Application.Contracts/Features/Carts/Commands/RemoveProductFromCart/RemoveProductFromCartRequestBodyDto.cs
@@ -1,3 +1,4 @@
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace Application.Contracts.Features.Carts.Commands.RemoveProductFromCart;
 
 public sealed record RemoveProductFromCartRequestBodyDto
--- Application/Features/Categories/Commands/RemoveCategory/RemoveCategoryHandler.cs
+++ Application/Features/Categories/Commands/RemoveCategory/RemoveCategoryHandler.cs
@@ -1,11 +1,13 @@
 using Application.Contracts.Features.Categories.Commands.RemoveCategory;
+using Domain.Entities.Products.Parameters;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Contracts;
 
 namespace Application.Features.Categories.Commands.RemoveCategory;
 
-internal sealed class RemoveCategoryHandler(IDbContext context) : IRequestHandler<RemoveCategoryCommand>
+internal sealed class RemoveCategoryHandler(IDbContext context, TimeProvider timeProvider) :
+    IRequestHandler<RemoveCategoryCommand>
 {
     public async Task Handle(RemoveCategoryCommand request, CancellationToken cancellationToken)
     {
@@ -13,8 +15,11 @@ internal sealed class RemoveCategoryHandler(IDbContext context) : IRequestHandle
             .AsTracking()
             .SingleOrDefaultAsync(c => c.Id == request.RouteDto.CategoryId, cancellationToken);
         if (ReferenceEquals(category, default)) return;
-        
-        context.Categories.Remove(category);
+
+        category.Remove(new RemoveProductParameters
+        {
+            TimeProvider = timeProvider
+        });
 
         await context.SaveChangesAsync(cancellationToken);
     }
