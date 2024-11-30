b80b105dc0fa7965871994b276081681154aeba7
Vladislav Zainullin <mittoface02@mail.ru>
2024-11-30 22:53:46 +0300

    Добавляет каскадное удаление в запрос удаления продукта

--- Application/Features/Products/Commands/RemoveProduct/RemoveProductHandler.cs
+++ Application/Features/Products/Commands/RemoveProduct/RemoveProductHandler.cs
@@ -1,4 +1,5 @@
 using Application.Contracts.Features.Products.Commands.RemoveProduct;
+using Domain.Entities.Products;
 using Domain.Entities.Products.Parameters;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -10,15 +11,28 @@ internal sealed class RemoveProductHandler(IDbContext context, TimeProvider time
 {
     public async Task Handle(RemoveProductCommand request, CancellationToken cancellationToken)
     {
-        var product = await context.Products
-            .AsTracking()
-            .SingleOrDefaultAsync(p => p.Id == request.RouteDto.ProductId, cancellationToken);
+        var product = await GetProductAsync(request.RouteDto.ProductId, cancellationToken);
+        
         if (ReferenceEquals(product, default)) return;
         
         product.Remove(new RemoveProductParameters
         {
             TimeProvider = timeProvider
         });
+        
         await context.SaveChangesAsync(cancellationToken);
+    }
+
+    private Task<Product?> GetProductAsync(Guid productId, CancellationToken cancellationToken)
+    {
+        return context.Products
+            .AsTracking()
+            .Include(static p => p.Characteristics)
+            .Include(static p => p.ProductInCarts)
+            .Include(static p => p.ProductInCategories)
+            .Include(static p => p.ProductInOrders)
+            .Include(static p => p.Favorites)
+            .AsSplitQuery()
+            .SingleOrDefaultAsync(p => p.Id == productId, cancellationToken);
     }
 }
